319cf07e7142eb30ab58ed0249422753db9dc81a
xiaodong <15053122880@163.com>
2019-12-11 15:49:09 +0800

    first commit

---
+++ src/main/java/com/gupao/thread/executors/ExecutorsApplication.java
@@ -0,0 +1,13 @@
+package com.gupao.thread.executors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExecutorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gupao/thread/executors/ExecutorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gupao.thread.executors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
